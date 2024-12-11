c05400fc32bc91a44c925c701233fc4ee4bbf0dc
k-ibaraki <ibaraki@ncdc.co.jp>
2024-12-11 20:30:34 +0900

    claude3.5haiku

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,8 +3,8 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
   chat: a.conversation({
-    //aiModel: a.ai.model('Claude 3.5 Haiku'),
-    aiModel: a.ai.model('Amazon Nova Lite'),
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    //aiModel: a.ai.model('Amazon Nova Lite'),
     systemPrompt: 'You are a helpful assistant',
   })
     .authorization((allow) => allow.owner()),
